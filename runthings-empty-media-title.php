c178b32018193d406ac75b226f74478e8303afe4
Matthew Harris <rtpharry@hotmail.com>
2022-01-06 15:07:35 +0000

    docs(meta): update plugin meta to final plugin name

--- runthings-empty-media-title.php
+++ runthings-empty-media-title.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Clear Media Title On Upload
+Plugin Name: Empty Media Title On Upload
 Plugin URI: https://runthings.dev
-Description: Sets the image's title attribute to an empty field on upload, instead of setting it to the filename
+Description: Sets the image title attribute to an empty field on upload, instead of setting it to the filename
 Version: 1.0.0
 Author: Matthew Harris, runthings.dev
 Author URI: https://runthings.dev/
